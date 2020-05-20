e08c562dfb26254e958d2a34306c48e384c15456
Antoinette <create@aplusmedia.info>
2020-05-19 21:06:39 -0400

    confused

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
-const Project = require("../node-api-challenge/data/helpers/projectModel");
-const Action = require ("../node-api-challenge/data/helpers/actionModel");
+const Project = require("../data/helpers/projectModel.js");
+const Action = require ("../data/helpers/actionModel.js");
 const express = require("express");
 
 const server = express();
@@ -22,43 +22,33 @@ server.use(express.json());
 
 const PORT = 5000;
 
+
+
+// server.get('/', (req, res) => {
+//     res.send("Hello Antoinette");
+// })
+
+server.get("api/projectModel", (req, res) => {
+    Project.res(req.query)
+  .then(data => {
+    res.status(200).send('data');
+})
+.catch(err => {
+    console.log(err)
+    res.status(500).json({message: "ERROR"})
+})
+
+server.post('/api/projects', (req, res) => {
+    res.status(201).json({url: 'api/projects', operation: 'POST'})
+})
+
+server.put('/api/projects', (req, res) => {
+    res.status(200).json({url: '/api/projects', operation: 'PUT'})
+})
+
+server.delete('/api/projects', (req, res) => {
+    res.status(204);
+})
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 })
-
-server.get('/', (req, res) => {
-    res.send("Hello Antoinette");
-})
-
-server.get("actions", (req, res) => {
-    // const id = req.params.id
-    Action.get(req.query)
-    .then(data => {
-        if(data){
-            res.status(200).json(data)
-        } else {
-            res.status(404).json({
-                message: "The post with the specified ID does not exist."
-            })
-        }
-    })
-    .catch(err => {
-        console.log({err})
-        res.status(500).json({
-            error: "The information could not be retrieved."
-        })
-    })
-})
-
-server.get("/actions/description", (req, res) => {
-    Action.get(req.query)
-    .then(data => {
-        res.status(200).json(data)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({message: "Error"})
-    })
-})
-
-server.post("")
